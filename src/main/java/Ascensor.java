92b6d16b2f604a9e6e86b03dfb544bd9090a9ea3
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-10-25 18:50:55 -0400

    se creo la clase de la cantidad de persona

--- src/main/java/Ascensor.java
+++ src/main/java/Ascensor.java
@@ -1,2 +1,18 @@
 public class Ascensor {
+
+    private Character[] pisoEdificio ={'0', '0', '0'};
+    private final int LIMITE_MAXIMO_PERSONAS=1;
+
+    public void crearPersona(Integer cantidadPersona, Integer pisoActual, Integer pisoDestino) throws Exception{
+        verificarCantidadPersonas(cantidadPersona);
+
+    }
+
+    private void verificarCantidadPersonas(Integer personas) throws Exception {
+        if(personas>LIMITE_MAXIMO_PERSONAS){
+            throw new Exception();
+        }
+    }
+
+
 }

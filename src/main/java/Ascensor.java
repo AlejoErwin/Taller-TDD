d43ffb6cf6aa257964622b454415e16837998132
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-10-25 20:44:39 -0400

    se creo Ascensor

--- src/main/java/Ascensor.java
+++ src/main/java/Ascensor.java
@@ -2,10 +2,15 @@ public class Ascensor {
 
     private int[] pisoDeEdificio = {0,0,0};
     private final int LIMITE_MAXIMA_ASCENSOR = 1;
+    private int pisoActualAscensor = 0;
+    private int pisoDestinoAscensor = 0;
+    private String estadoPuerta = "cerrado";
 
-    public void PresionarBotonAscensor(int personas, int pisoDestino)throws Exception{
+    public String PresionarBotonAscensor(int personas, int pisoDestino)throws Exception{
         verificarCapacidad(personas);
         int pisoActual =crearPersona(pisoDestino);
+        int piso = posicionAscensor(pisoActual, pisoDestino);
+        return "El ascensor esta en el piso: "+piso;
     }
     private void verificarCapacidad(int cantidadPersonas) throws Exception{
         if(cantidadPersonas>LIMITE_MAXIMA_ASCENSOR)
@@ -31,6 +36,28 @@ public class Ascensor {
         if(pisoDestino>pisoDeEdificio.length || pisoDestino<1){
             throw new Exception("Piso inexistente");
         }
+    }
+
+    private int posicionAscensor(int pisoActual, int pisoDestino){
+        String mensaje = "";
+        //if(pisoActualAscensor+1 == pisoActual && pisoDestinoAscensor == 0){
+        //    mensaje = "Abrir inmediatamente";
+        //}else{
+        //    mensaje = "Esperando ascensor";
+        //}
+        pisoActualAscensor = pisoActual;
+        estadoPuerta = "abierto";
+        pisoDestinoAscensor = pisoDestino;
+        estadoPuerta = "cerrado";
+        limpiarPisoEdificio();
+        pisoActualAscensor = pisoDestino - 1;
+        return pisoActualAscensor;
+    }
+    private void limpiarPisoEdificio(){
+        for(int i = 0; i < pisoDeEdificio.length ; i++){
+            pisoDeEdificio [i] = 0 ;
+        }
+        pisoDestinoAscensor = 0;
     }
 
 
